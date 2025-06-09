b83c5c985f29c2ad0c040af6d5f259cd689b68f9
FabLeo451 <fab.leo@gmail.com>
2025-06-10 00:19:16 +0200

    Get news from database

--- app/page.js
+++ app/page.js
@@ -1,8 +1,17 @@
 import { cookies } from 'next/headers';
-import Link from 'next/link';
 import jwt from 'jsonwebtoken';
 import pkg from '@/package.json';
 import HomeLoggedIn from '@/components/HomeLoggedIn';
+import pool from '@/lib/db';
+
+const SCHEMA = process.env.DB_SCHEMA;
+
+async function getNews() {
+	const res = await pool.query(`SELECT id, created, text FROM ${SCHEMA}.NEWS ORDER BY created DESC`);
+
+	//console.log('[home]', res.rows)
+	return res.rows;
+}
 
 export default async function Home() {
 
@@ -25,6 +34,8 @@ export default async function Home() {
 		//return (<div className="pl-1">Invalid token: {err.message}</div>);
 	}
 
+	const news = await getNews();
+
 	return (
 		<div>
 
@@ -32,7 +43,7 @@ export default async function Home() {
 			{loggedIn ? (
 				<HomeLoggedIn />
 			) : (
-				<div className="relative min-h-screen flex flex-col text-center px-6 py-[6em] border">
+				<div className="relative min-h-screen flex flex-col text-center px-6 py-[6em]">
 					<div className="flex flex-col items-center gap-4">
 						<h1 className="text-5xl md:text-6xl font-extrabold text-white neon-text drop-shadow-md animate-pulse">
 							ekhoes
@@ -45,15 +56,15 @@ export default async function Home() {
 					<div className="mt-[3em] mb-[1em] text-white text-lg">News</div>
 
 					<div className="card w-full max-w-[90vw] lg:max-w-[50vw] mx-auto bg-white/10 border border-white/10 backdrop-blur-md shadow-2xl shadow-black/40">
-						<div className="card-body text-left">
-							<div className="flex gap-2 items-baseline">
-								<span className="text-xs text-gray-400">09/06/2025</span>
-								<span className="text-gray-300">This is a fake news</span>
-							</div>
-							<div className="flex gap-2 items-baseline">
-								<span className="text-xs text-gray-400">08/06/2025</span>
-								<span className="text-gray-300">This is another fake news, bu in this case it's longer than the other</span>
-							</div>
+						<div className="card-body text-left flex flex-col gap-3">
+							{news.map((item) => (
+								<div key={item.id} className="flex gap-2 items-baseline">
+									<span className="text-xs text-gray-400">
+										{new Date(item.created).toLocaleDateString()}
+									</span>
+									<span className="text-gray-300">{item.text}</span>
+								</div>
+							))}
 						</div>
 					</div>
 
