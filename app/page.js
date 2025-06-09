3c6fc47ae5243e8668126ffadd80fa4c1f2605df
FabLeo451 <fab.leo@gmail.com>
2025-06-09 22:53:53 +0200

    Added animation to logo

--- app/page.js
+++ app/page.js
@@ -5,52 +5,52 @@ import HomeLoggedIn from '@/components/HomeLoggedIn';
 
 export default async function Home() {
 
-  const version = pkg.version || '1.0.0'; // fallback
+	const version = pkg.version || '1.0.0'; // fallback
 
-  const cookieStore = await cookies();
-  const token = cookieStore.get(process.env.COOKIE_NAME)?.value;
+	const cookieStore = await cookies();
+	const token = cookieStore.get(process.env.COOKIE_NAME)?.value;
 
-  let loggedIn = false;
-  let decoded;
+	let loggedIn = false;
+	let decoded;
 
-  try {
-    decoded = jwt.verify(token, process.env.JWT_SECRET);
+	try {
+		decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    //console.log('[Navbar] decoded = ', decoded)
+		//console.log('[Navbar] decoded = ', decoded)
 
-    loggedIn = true;
+		loggedIn = true;
 
-  } catch (err) {
-    //return (<div className="pl-1">Invalid token: {err.message}</div>);
-  }
+	} catch (err) {
+		//return (<div className="pl-1">Invalid token: {err.message}</div>);
+	}
 
-  return (
-    <div>
+	return (
+		<div>
 
-      {/* Contenuto centrale */}
-      {loggedIn ? (
-        <HomeLoggedIn />
-      ) : (
-        <div className="relative h-screen flex flex-col items-center justify-center text-center px-6 py-6">
-          {/* Contenuto centrato verticalmente */}
-          <div className="flex flex-col items-center gap-4">
-            <h1 className="text-5xl md:text-6xl font-extrabold text-white neon-text drop-shadow-md">
-              ekhoes
-            </h1>
-            <p className="text-lg md:text-xl text-gray-400">
-              start a digital journey
-            </p>
-          </div>
+			{/* Contenuto centrale */}
+			{loggedIn ? (
+				<HomeLoggedIn />
+			) : (
+				<div className="relative h-screen flex flex-col items-center justify-center text-center px-6 py-6">
+					{/* Contenuto centrato verticalmente */}
+					<div className="flex flex-col items-center gap-4">
+						<h1 className="text-5xl md:text-6xl font-extrabold text-white neon-text drop-shadow-md animate-pulse">
+							ekhoes
+						</h1>
+						<p className="text-lg md:text-xl text-gray-400">
+							work in progres...
+						</p>
+					</div>
 
-          {/* Footer ancorato in basso */}
-          <div className="absolute bottom-4 text-s text-gray-600">
-            Version {version}
-          </div>
-        </div>
-      )
-      }
+					{/* Footer ancorato in basso */}
+					<div className="absolute bottom-4 text-s text-gray-600">
+						Version {version}
+					</div>
+				</div>
+			)
+			}
 
 
-    </div>
-  );
+		</div>
+	);
 }
