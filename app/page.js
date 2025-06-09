0e051045fea032aeac7b907dec840ed59d61e2ad
FabLeo451 <fab.leo@gmail.com>
2025-06-09 23:45:39 +0200

    Added news section

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import { cookies } from 'next/headers';
+import Link from 'next/link';
 import jwt from 'jsonwebtoken';
 import pkg from '@/package.json';
 import HomeLoggedIn from '@/components/HomeLoggedIn';
@@ -31,8 +32,7 @@ export default async function Home() {
 			{loggedIn ? (
 				<HomeLoggedIn />
 			) : (
-				<div className="relative h-screen flex flex-col items-center justify-center text-center px-6 py-6">
-					{/* Contenuto centrato verticalmente */}
+				<div className="relative min-h-screen flex flex-col text-center px-6 py-[6em] border">
 					<div className="flex flex-col items-center gap-4">
 						<h1 className="text-5xl md:text-6xl font-extrabold text-white neon-text drop-shadow-md animate-pulse">
 							ekhoes
@@ -42,10 +42,22 @@ export default async function Home() {
 						</p>
 					</div>
 
-					{/* Footer ancorato in basso */}
-					<div className="absolute bottom-4 text-s text-gray-600">
-						Version {version}
+					<div className="mt-[3em] mb-[1em] text-white text-lg">News</div>
+
+					<div className="card w-full max-w-[90vw] lg:max-w-[50vw] mx-auto bg-white/10 border border-white/10 backdrop-blur-md shadow-2xl shadow-black/40">
+						<div className="card-body text-left">
+							<div className="flex gap-2 items-baseline">
+								<span className="text-xs text-gray-400">09/06/2025</span>
+								<span className="text-gray-300">This is a fake news</span>
+							</div>
+							<div className="flex gap-2 items-baseline">
+								<span className="text-xs text-gray-400">08/06/2025</span>
+								<span className="text-gray-300">This is another fake news, bu in this case it's longer than the other</span>
+							</div>
+						</div>
 					</div>
+
+
 				</div>
 			)
 			}
