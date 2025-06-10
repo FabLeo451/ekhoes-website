2d1f12c4f678da89925ddb676f193e8d83b52020
FabLeo451 <fab.leo@gmail.com>
2025-06-10 13:45:44 +0200

    Limit news select

--- app/page.js
+++ app/page.js
@@ -5,9 +5,10 @@ import HomeLoggedIn from '@/components/HomeLoggedIn';
 import pool from '@/lib/db';
 
 const SCHEMA = process.env.DB_SCHEMA;
+const MAX_NEWS = process.env.MAX_NEWS || 10;
 
 async function getNews() {
-	const res = await pool.query(`SELECT id, created, text FROM ${SCHEMA}.NEWS ORDER BY created DESC`);
+	const res = await pool.query(`SELECT id, created, text FROM ${SCHEMA}.NEWS ORDER BY created DESC LIMIT ${MAX_NEWS}`);
 
 	//console.log('[home]', res.rows)
 	return res.rows;
@@ -60,7 +61,7 @@ export default async function Home() {
 							{news.map((item) => (
 								<div key={item.id} className="flex gap-2 items-baseline">
 									<span className="text-xs text-gray-400">
-										{new Date(item.created).toLocaleDateString()}
+										{new Date(item.created).toLocaleString()}
 									</span>
 									<span className="text-gray-300">{item.text}</span>
 								</div>
