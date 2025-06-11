aadca15f6f3861525d6def47a145d37b92798b7d
FabLeo451 <fab.leo@gmail.com>
2025-06-12 00:13:31 +0200

    Configured for local test

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  allowedDevOrigins: ['http://192.168.1.126:3001'],
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
+
