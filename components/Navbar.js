ca53478b72986d733d60f9fb16b5529ef6618daf
FabLeo451 <fab.leo@gmail.com>
2025-12-24 13:11:43 +0100

    Login and Sign up buttons inverted in the navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -29,12 +29,12 @@ export default async function Navbar() {
                             </div>
                         ) : (
                             <>
-                            <a href="/login">
-                                <button className="btn btn-primary">Log in</button>
-                            </a>
-                            {<Link href="/sign-up">
-                                <button className="btn btn-soft ml-[1em]">Sign up</button>
-                            </Link>}
+                                {<Link href="/sign-up">
+                                    <button className="btn btn-soft">Sign up</button>
+                                </Link>}
+                                <a href="/login">
+                                    <button className="btn btn-primary ml-[1em]">Log in</button>
+                                </a>
                             </>
                         )}
                     </div>
