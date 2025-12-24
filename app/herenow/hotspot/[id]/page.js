04aa9e85c963c4761c8d269cb2f1ae613f5cbca2
FabLeo451 <fab.leo@gmail.com>
2025-12-24 13:07:06 +0100

    Comments added

--- app/herenow/hotspot/[id]/page.js
+++ app/herenow/hotspot/[id]/page.js
@@ -9,6 +9,7 @@ export default function Hotspot({ params }) {
 	const [hotspots, setHotspots] = useState([]);
 	const [notFound, setNotFound] = useState(false);
 	const [loaded, setLoaded] = useState(false);
+	const [comments, setComments] = useState([]);
 
 	useEffect(() => {
 		if (!id) return;
@@ -22,7 +23,7 @@ export default function Hotspot({ params }) {
 				return res.json();
 			})
 			.then((data) => {
-				console.log(data);
+				//console.log(data);
 				if (data) {
 					setHotspots(data);
 					setLoaded(true);
@@ -33,6 +34,28 @@ export default function Hotspot({ params }) {
 				setNotFound(true);
 			});
 	}, [id]);
+
+	useEffect(() => {
+		if (!loaded) return;
+
+		getComments();
+	}, [loaded]);
+
+	async function getComments() {
+		fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/hotspot/${id}/comments`)
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				console.log(data);
+				if (data) {
+					setComments(data);
+				}
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}
 
 	if (notFound) return <div>Hotspot not found (404)</div>;
 	if (!loaded) return <div>Loading...</div>;
@@ -71,14 +94,40 @@ export default function Hotspot({ params }) {
 				</div>
 
 				{/* Descrizione: dimensione media, line-height maggiore */}
-				<div style={{
-					fontSize: '1.1rem',
-					lineHeight: '1.6',
-					color: '#eee', // quasi bianco
-				}}>
-					{hotspots[0].description}
+				<div className="w-full rounded-sm lg:max-w-[50vw] mx-auto  bg-white/10 border border-white/10 backdrop-blur-md shadow-2xl shadow-black/40">
+					<div className="p-2 text-left flex flex-col">
+						{hotspots[0].description}
+					</div>
 				</div>
 			</div>
+
+			{/* Comments */}
+			<div className="text-left flex flex-col gap-2 pr-2 ml-5 mr-5 mt-3 mb-5 pb-5">
+				<div className="font-bold text-base mb-1">Comments</div>
+
+				<div className="max-h-[40vh] overflow-y-auto">
+					{comments.map((item) => (
+						<div key={item.id} className="flex flex-col mb-3">
+
+							<div className="flex gap-1 items-baseline text-xs text-gray-400">
+								<span className="font-bold text-gray-300">
+									{item.userName}
+								</span>
+								<span>
+									{new Date(item.created).toLocaleString()}
+								</span>
+							</div>
+
+							<div className="text-gray-300 text-xs">
+								{item.message}
+							</div>
+
+						</div>
+					))}
+				</div>
+			</div>
+
+
 
 		</div>
 	);
